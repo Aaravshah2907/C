0301af4d9954be142ccc5e94dac5b09619056a66
Aarav Shah <aaravshah0011@gmail.com>
2024-04-03 19:54:24 +0530

    Update

--- myprogs/Practice_progs/prac_string/student_database.c
+++ myprogs/Practice_progs/prac_string/student_database.c
@@ -15,34 +15,93 @@ int main(int argc, char const *argv[])
 		char id_temp[15], name_temp[30];
 		printf("Enter Id:    ");
 		scanf("%s", id_temp);
-		for (int j = 0; j < 15; j++)
-		{
-			id_temp[j] = ID[i][j];
-		}
+		int len = strlen(id_temp);
+		int j = 0;
+		for (; j < len && (isalpha(id_temp[j]) || isdigit(id_temp[j])); j++)
+			ID[i][j] = id_temp[j];
+		ID[i][j] = '\0';
+
 		printf("Enter Name:  ");
 		scanf("%s", name_temp);
 		for (int x = 0; x < 30; ++x)
-			name_temp[x] = Name[i][x];
+			Name[i][x] = name_temp[x];
 
 		printf("Enter Marks: ");
 		scanf("%d", &Marks[i]);
 	}
 
-	for (int i = 0; i < st_number; i++)
-	{
-		// printf();
-		// printf();
-		// printf();
-	}
+	// // For sorting on basis of name
+	// for (int i = 1; i < st_number; ++i)
+	// {
+	// 	for (int j = 0; j < st_number - i; ++j)
+	// 	{
+	// 		if (strcmp(Name[j], Name[j + 1]) > 0)
+	// 		{
+	// 			// Marks Swap
+	// 			int temp = Marks[j];
+	// 			Marks[j] = Marks[j + 1];
+	// 			Marks[j + 1] = temp;
 
-	for (int i = 0; i < st_number; ++i)
+	// 			// Name Swap
+	// 			char n1[30];
+	// 			for (int x = 0; x < 30; ++x)
+	// 				n1[x] = Name[j][x];
+	// 			for (int x = 0; x < 30; ++x)
+	// 				Name[j][x] = Name[j + 1][x];
+	// 			for (int x = 0; x < 30; ++x)
+	// 				Name[j + 1][x] = n1[x];
+
+	// 			// ID swap
+	// 			char id_temp[15];
+	// 			for (int x = 0; x < 15; ++x)
+	// 				id_temp[x] = ID[j][x];
+	// 			for (int x = 0; x < 15; ++x)
+	// 				ID[j][x] = ID[j + 1][x];
+	// 			for (int x = 0; x < 15; ++x)
+	// 				ID[j + 1][x] = id_temp[x];
+	// 		}
+	// 	}
+	// }
+
+	// // For sorting on basis of ID
+	// for (int i = 1; i < st_number; ++i)
+	// {
+	// 	for (int j = 0; j < st_number - i; ++j)
+	// 	{
+	// 		if (strcmp(ID[j], ID[j + 1]) > 0)
+	// 		{
+	// 			// Marks Swap
+	// 			int temp = Marks[j];
+	// 			Marks[j] = Marks[j + 1];
+	// 			Marks[j + 1] = temp;
+
+	// 			// Name Swap
+	// 			char n1[30];
+	// 			for (int x = 0; x < 30; ++x)
+	// 				n1[x] = Name[j][x];
+	// 			for (int x = 0; x < 30; ++x)
+	// 				Name[j][x] = Name[j + 1][x];
+	// 			for (int x = 0; x < 30; ++x)
+	// 				Name[j + 1][x] = n1[x];
+
+	// 			// ID swap
+	// 			char id_temp[15];
+	// 			for (int x = 0; x < 15; ++x)
+	// 				id_temp[x] = ID[j][x];
+	// 			for (int x = 0; x < 15; ++x)
+	// 				ID[j][x] = ID[j + 1][x];
+	// 			for (int x = 0; x < 15; ++x)
+	// 				ID[j + 1][x] = id_temp[x];
+	// 		}
+	// 	}
+	// }
+
+	// For sorting on basis of marks
+	for (int i = 1; i < st_number; ++i)
 	{
 		for (int j = 0; j < st_number - i; ++j)
 		{
-			char *p1, *p2;
-			p1 = &(Name[0][0]) + (j * 15) - 1;
-			p2 = &(Name[0][0]) + (j * 15 + 14);
-			if (strcmp(p1, p2) > 0)
+			if ((Marks[j] > Marks[j + 1]) > 0)
 			{
 				// Marks Swap
 				int temp = Marks[j];
@@ -56,7 +115,7 @@ int main(int argc, char const *argv[])
 				for (int x = 0; x < 30; ++x)
 					Name[j][x] = Name[j + 1][x];
 				for (int x = 0; x < 30; ++x)
-					Name[j][x] = n1[x];
+					Name[j + 1][x] = n1[x];
 
 				// ID swap
 				char id_temp[15];
@@ -65,27 +124,14 @@ int main(int argc, char const *argv[])
 				for (int x = 0; x < 15; ++x)
 					ID[j][x] = ID[j + 1][x];
 				for (int x = 0; x < 15; ++x)
-					ID[j][x] = id_temp[x];
+					ID[j + 1][x] = id_temp[x];
 			}
 		}
 	}
 
+	printf("Printing it nicely: \n\n\n\n\n\n");
+
 	for (int i = 0; i < st_number; ++i)
-	{
-		printf("ID: ");
-		for (int j = 0; j < 15; j++)
-		{
-			printf("%c", ID[i][j]);
-		}
-
-		printf("	Name: ");
-		for (int j = 0; j < 30; j++)
-		{
-			printf("%c", Name[i][j]);
-		}
-
-		printf("Marks:%d", Marks[i]);
-		printf("\n");
-	}
+		printf("%d.\n\tID: %s\n\tName: %s\n\tMarks:%d\n", i + 1, ID[i], Name[i], Marks[i]);
 	return 0;
 }
